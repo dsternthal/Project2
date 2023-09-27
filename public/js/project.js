ff9981c926750a4a01db45c304d1fe62e2241aab
Isa Vega <imvega100@gmail.com>
2023-09-26 21:50:43 -0400

    added put route

--- public/js/project.js
+++ public/js/project.js
@@ -1,8 +1,18 @@
 const markComplete = async (event) => {
-    const completeEl = document.querySelector('#complete')
-    if(event.target.hasAttribute('#importance')) {
-        
-    }
+    const id = event.target.getAttribute('data-id')
+    console.log(id)
+    const response = await fetch('/api/projects/' + id, {
+        method: 'PUT',
+        body: JSON.stringify({ importance:"gray" }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
 
 }
 
